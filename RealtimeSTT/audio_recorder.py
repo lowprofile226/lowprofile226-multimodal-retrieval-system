f37328bb16c5f004cd9fac4b17df9ab07160f0ab
lowprofile226 <2253626236@qq.com>
2024-07-18 01:23:12 +0800

    适应网页版

--- RealtimeSTT/audio_recorder.py
+++ RealtimeSTT/audio_recorder.py
@@ -95,7 +95,7 @@ class AudioToTextRecorder:
                  compute_type: str = "default",
                  input_device_index: int = None,
                  gpu_device_index: Union[int, List[int]] = 0,
-                 device: str = "cuda",
+                 device: str = "cpu",#"cuda",
                  on_recording_start=None,
                  on_recording_stop=None,
                  on_transcription_start=None,
